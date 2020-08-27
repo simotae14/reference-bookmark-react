35b6dc75fef1d280396cb746d1c9ae48f03f198f
Simone <startae14@gmail.com>
2020-08-28 00:08:56 +0200

    create a fClass component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const App = () => (
-  <div className="container">
-    <h1>Hello World</h1>
-    <h2>Hey Guys</h2>
-  </div> 
-);
+// const App = () => (
+//   <div className="container">
+//     <h1>Hello World</h1>
+//     <h2>Hey Guys</h2>
+//   </div> 
+// );
+
+// Class Component
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello World</h1>
+        <h2>Hey Guys</h2>
+      </div>
+    );
+  }
+}
 
 export default App;
