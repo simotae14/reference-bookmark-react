841bf907e2ef91521eeb5a6f34ef522d529144ff
Simone <startae14@gmail.com>
2020-08-29 00:09:35 +0200

    call an arrow function that returns an arrow function

--- src/App.js
+++ src/App.js
@@ -20,17 +20,24 @@ class App extends React.Component {
     counter: 0
   };
   
-  increment = (num) => {
+  increment = (num) => () => {
     this.setState({
       counter: this.state.counter + num
     });
   }
+  // increment = (num) => {
+  //   return () => {
+  //     this.setState({
+  //       counter: this.state.counter + num
+  //     });
+  //   }
+  // }
   render() {
     return (
       <div className="container">
         <h1>Counter Application: {this.state.counter}</h1>
-        <button onClick={() => this.increment(1)}>Increment</button>
-        <button onClick={() => this.increment(-1)}>Decrement</button>
+        <button onClick={this.increment(1)}>Increment</button>
+        <button onClick={this.increment(-1)}>Decrement</button>
       </div>
     );
   }
